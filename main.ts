e8c35eeb2d4bd3329746aa5797b6f6315aec595f
Marion2010 <67765921+Marion2010@users.noreply.github.com>
2020-07-02 19:32:07 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    hand = randint(1, 3)
+})
+basic.forever(function () {
+	
+})
